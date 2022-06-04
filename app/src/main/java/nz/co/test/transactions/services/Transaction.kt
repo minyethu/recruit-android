ed891419bcbe00feb8bc5cbbd917995bdde0bb98
minthu <min.thu@eroad.com>
2022-06-04 16:40:04 +1200

    Fix transactionDate to LocalDateTime to match with API response

--- app/src/main/java/nz/co/test/transactions/services/Transaction.kt
+++ app/src/main/java/nz/co/test/transactions/services/Transaction.kt
@@ -1,12 +1,13 @@
 package nz.co.test.transactions.services
 
+import com.squareup.moshi.Json
 import java.math.BigDecimal
-import java.time.OffsetDateTime
+import java.time.LocalDateTime
 
 data class Transaction(
-    val id: Int,
-    val transactionDate: OffsetDateTime,
-    val summary: String,
-    val debit: BigDecimal,
-    val credit: BigDecimal
+    @Json(name = "id") val id: Int,
+    @Json(name = "transactionDate") val transactionDate: LocalDateTime,
+    @Json(name = "summary") val summary: String,
+    @Json(name = "debit") val debit: BigDecimal,
+    @Json(name = "credit") val credit: BigDecimal
 )
